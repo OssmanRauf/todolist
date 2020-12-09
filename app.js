b80537e74bff6fe292f4290ae5b0cade1ffc978a
ossman rauf <ossmanrauf8@gmail.com>
2020-12-09 18:36:16 +0200

    second commit

--- app.js
+++ app.js
@@ -7,80 +7,48 @@ const task_list = document.querySelector(".task-list");
 btn.addEventListener("click", () => {
     if (input.value.replace(/ /g, "")) {
         const task = input.value.trim();
+        const task_formated = task.charAt(0).toUpperCase() + task.slice(1);
         const li = document.createElement("li");
-        li.innerHTML = `<h4 class="task">${task}</h4>
+        li.innerHTML = `<h4 class="task">${task_formated}</h4>
                 <img class="check-img" src="./check_icon.svg ">
                 <img class="trash-img" src="./trash_icon.svg">`;
         task_list.appendChild(li);
         input.value = "";
     } else {
-        alert("Por favor coloque um valor certo");
+        alert("Por favor coloque um texto valido");
         input.value = "";
     }
 });
 
-task_list.addEventListener('click', () => {;
+task_list.addEventListener('click', () => {
+    const task_text = event.target.parentElement.firstChild;
+    const li = event.target.parentElement;
+    if (event.target.classList.contains('check-img')) {
+        if (task_text.classList.contains('compleated')) {
+            event.target.classList.remove("checked");
+            task_text.classList.remove("compleated");
+
+
+        } else {
+            event.target.classList.add('checked');
+            task_text.classList.add("compleated");
+        };
+    };
+});
+task_list.addEventListener("click", () => {
+
     const task = event.target.parentElement.firstChild;
-    if (task.classList.contains('compleated')) {
-        event.target.classList.remove("checked");
-        task.classList.remove("compleated");
-    } else {
-        event.target.classList.add('checked');
-        task.classList.add("compleated");
-    }
+    const li = event.target.parentElement;
+    if (event.target.classList.contains("trash-img")) {
+        if (event.target.previousElementSibling.classList.contains("checked")) {
+            if (task) {
+                li.remove();
+            }
+        } else if (confirm("Are you sure you want to delete this task?")) {
+            if (task) {
+                li.remove();
+            };
+        };
 
-})
-
-// function task_done() {
-//     const check_icon = document.querySelectorAll('.task').this;
-//     // if (check_icon.classList.contains("checked")) {
-//     //     check_icon.classList.remove("checked");
-//     //     check_icon.previousElementSibling.classList.remove("compleated");
-//     // } else {
-//     //     check_icon.classList.add("checked");
-//     //     check_icon.previousElementSibling.classList.add("compleated");
-//     // };
-//     const h = document.querySelectorAll(".task");
-//     console.log(this.siblings('.task').text());
-
-// }
-// task_list.addEventListener('click', (element) => {
-//     const task = element.parentElement.previousSibling;
-//     if (element.classList.contains("checked")) {
-//         task.classList.remove("compleated");
-//         element.classList.remove("checked");
-//     } else {
-//         task.classList.add("compleated");
-//         element.classList.add("checked");
-//     }
-// });
-// function click() {
-//     if (input.value.replace(/ /g, "")) {
-//         const task = input.value.trim();
-//         const formated_task = task.replace(task.charAt(0).toUpperCase());
-//         const li = document.createElement("li");
-//         console.log(task);
-//         li.innerHTML = `<h4 class="task">${formated_task}</h4><span><img class="check-img" src="./check_icon.svg" ><img class="trash-img" src="./trash_icon.svg"></span>`;
-//         task_list.appendChild(li);
-//         input.value = "";
-//     } else {
-//         alert("Por favor coloque um valor certo");
-//         input.value = "";
-//     }
-//     // return (check = document.querySelectorAll(".check-img"));
-
-// };
-
-// check.forEach(element => {
-//     element.addEventListener('click', () => {
-//         const task = element.parentElement.previousSibling;
-//         if (element.classList.contains('checked')) {
-//             task.classList.remove("compleated");
-//             element.classList.remove('checked');
-
-//         } else {
-//             task.classList.add('compleated');
-//             element.classList.add("checked");
-//         };
-//     });
-// });
+    };
+});
