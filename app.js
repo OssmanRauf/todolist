e4228245b5566b674f4cf0035caf27404f5fa659
ossman rauf <ossmanrauf8@gmail.com>
2021-01-06 20:34:16 +0200

    'orders and detail'

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const input = document.querySelector("#todo-input");
 const form = document.querySelector("#form");
-const task_list = document.querySelector(".task-list");
+const taskList = document.querySelector(".task-list");
 const search = document.querySelector('#search');
 
+
+showAllElementsUI();
+
+
 class Task {
-    constructor(task, compleated) {
+    constructor(task, completed) {
         this.task = task;
-        this.compleated = compleated;
+        this.completed = completed;
     };
 };
-show_all_elements_ui()
+
 
 
 //creating task adding element in local storage and in the page
@@ -17,12 +21,12 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (input.value.replace(/ /g, "")) {
         const text = input.value.trim();
-        const task_formated = text.charAt(0).toUpperCase() + text.slice(1);
-        const task = new Task(task_formated, false);
+        const taskFormated = text.charAt(0).toUpperCase() + text.slice(1);
+        const task = new Task(taskFormated, false);
         // adding task to the page
-        add_element(task.task);
+        addElement(task.task);
         // adding task to local storage
-        add_localStorage(task);
+        addToLocalStorage(task);
         input.value = "";
     } else {
         alert("Please type a valid text");
@@ -32,31 +36,31 @@ form.addEventListener("submit", (e) => {
 
 
 // listens to click event in the entire task block
-task_list.addEventListener('click', () => {
+taskList.addEventListener('click', () => {
     let tasks;
     tasks = JSON.parse(localStorage.getItem("tasks"));
-    const task_text = event.target.parentElement.firstChild;
+    const taskText = event.target.parentElement.firstChild;
     const li = event.target.parentElement;
-    const index = get_index(task_text.textContent);
-    //if the check button was clicked add or remove compleated class
+    const index = getInd(taskText.textContent);
+    //if the check button was clicked add or remove completed class
     if (event.target.classList.contains("check-img")) {
-        if (task_text.classList.contains("compleated")) {
+        if (taskText.classList.contains("completed")) {
             event.target.classList.remove("checked");
-            task_text.classList.remove("compleated");
+            taskText.classList.remove("completed");
             li.style.backgroundColor = "rgba(255, 255, 255, 0.955)";
-            tasks[index].compleated = false;
+            tasks[index].completed = false;
             localStorage.setItem("tasks", JSON.stringify(tasks));
-            if (document.getElementById("options").value === "compleated") {
+            if (document.getElementById("options").value === "completed") {
                 li.parentElement.removeChild(li)
             }
 
         } else {
             event.target.classList.add("checked");
-            task_text.classList.add("compleated");
+            taskText.classList.add("completed");
             li.style.backgroundColor = "rgba(239, 239, 239, 0.9)";
-            tasks[index].compleated = true;
+            tasks[index].completed = true;
             localStorage.setItem("tasks", JSON.stringify(tasks));
-            if (document.getElementById("options").value === "incompleated") {
+            if (document.getElementById("options").value === "uncompleted") {
                 li.parentElement.removeChild(li);
             }
         };
@@ -65,7 +69,7 @@ task_list.addEventListener('click', () => {
     //if the trash button was clicked delete the task from UI and local storage
     if (event.target.classList.contains("trash-img")) {
         if (event.target.previousElementSibling.classList.contains("checked")) {
-            if (task_text) {
+            if (taskText) {
                 tasks.splice(index, 1);
                 localStorage.setItem("tasks", JSON.stringify(tasks));
                 li.remove();
@@ -81,12 +85,12 @@ task_list.addEventListener('click', () => {
 
 // search for a task
 search.addEventListener('keyup', () => {
-    const search_text = event.target.value.toLowerCase();
+    const searchText = event.target.value.toLowerCase();
     const li = document.querySelectorAll('li');
     li.forEach(element => {
         const task = element.firstChild.textContent.toLowerCase();
-        if (task.indexOf(search_text) != -1) {
-            console.log(task.indexOf(search_text));
+        if (task.indexOf(searchText) != -1) {
+            console.log(task.indexOf(searchText));
             element.style.display = "flex";
         } else {
             element.style.display = 'none';
@@ -100,7 +104,7 @@ search.addEventListener('keyup', () => {
 
 
 // get index(used to get index of a specific task)
-function get_index(text) {
+function getInd(text) {
     let tasks;
     tasks = JSON.parse(localStorage.getItem("tasks"));
     if (tasks.length != 0) {
@@ -111,101 +115,101 @@ function get_index(text) {
 };
 
 function clearUI() {
-    if (task_list.children.length > 0) {
-        task_list.innerHTML = '';
+    if (taskList.children.length > 0) {
+        taskList.innerHTML = '';
     }
 }
 
-function show_all_elements_ui() {
+function showAllElementsUI() {
     clearUI();
     if (localStorage.getItem("tasks") != null) {
         const tasks = JSON.parse(localStorage.getItem("tasks"));
         tasks.forEach((task) => {
             const li = document.createElement("li");
-            if (task.compleated === false) {
-                li.innerHTML = `<h4 class="task">${task.task}</h4><img class="check-img" src="./check_icon.svg"><img class="trash-img" src="./trash_icon.svg">`;
+            if (task.completed === false) {
+                li.innerHTML = `<h4 class="task">${task.task}</h4><img class="check-img" src="./img/check_icon.svg"><img class="trash-img" src="./img/trash_icon.svg">`;
             } else {
-                li.innerHTML = `<h4 class="task compleated">${task.task}</h4><img class="check-img checked" src="./check_icon.svg"><img class="trash-img" src="./trash_icon.svg">`;
+                li.innerHTML = `<h4 class="task completed">${task.task}</h4><img class="check-img checked" src="./img/check_icon.svg"><img class="trash-img" src="./img/trash_icon.svg">`;
                 li.style.backgroundColor = "rgba(239, 239, 239, 0.9)";
             }
-            task_list.insertBefore(li, task_list.firstChild);
+            taskList.insertBefore(li, taskList.firstChild);
         })
     }
 }
 
-function show_compleated_elements_ui() {
+function showCompletedElementsUI() {
     clearUI();
     if (localStorage.getItem("tasks") != null) {
         const tasks = JSON.parse(localStorage.getItem("tasks"));
         tasks.forEach((task) => {
-            if (task.compleated === true) {
+            if (task.completed === true) {
                 const li = document.createElement("li");
                 const h4 = document.createElement("h4");
                 h4.classList.add("task");
                 h4.textContent = task.task;
-                const check_btn = document.createElement("img");
-                check_btn.classList.add("check-img");
-                check_btn.src = "./check_icon.svg";
-                const trash_btn = document.createElement("img");
-                trash_btn.classList.add("trash-img");
-                trash_btn.src = "./trash_icon.svg";
-                h4.classList.add("compleated");
-                check_btn.classList.add("checked");
+                const checkBtn = document.createElement("img");
+                checkBtn.classList.add("check-img");
+                checkBtn.src = "./img/check_icon.svg";
+                const trashBtn = document.createElement("img");
+                trashBtn.classList.add("trash-img");
+                trashBtn.src = "./img/trash_icon.svg";
+                h4.classList.add("completed");
+                checkBtn.classList.add("checked");
                 li.style.backgroundColor = "rgba(239, 239, 239, 0.9)";
                 li.appendChild(h4);
-                li.appendChild(check_btn);
-                li.appendChild(trash_btn);
-                task_list.insertBefore(li, task_list.firstChild);
+                li.appendChild(checkBtn);
+                li.appendChild(trashBtn);
+                taskList.insertBefore(li, taskList.firstChild);
             }
         });
     }
 };
 
-function show_incompleated_elements_ui() {
+function showUncompletedElementsUI() {
     clearUI();
     if (localStorage.getItem("tasks") != null) {
         const tasks = JSON.parse(localStorage.getItem("tasks"));
         tasks.forEach((task) => {
-            if (task.compleated === false) {
+            if (task.completed === false) {
                 const li = document.createElement("li");
                 const h4 = document.createElement("h4");
                 h4.classList.add("task");
                 h4.textContent = task.task;
-                const check_btn = document.createElement("img");
-                check_btn.classList.add("check-img");
-                check_btn.src = "./check_icon.svg";
-                const trash_btn = document.createElement("img");
-                trash_btn.classList.add("trash-img");
-                trash_btn.src = "./trash_icon.svg";
+                const checkBtn = document.createElement("img");
+                checkBtn.classList.add("check-img");
+                checkBtn.src = "./img/check_icon.svg";
+                const trashBtn = document.createElement("img");
+                trashBtn.classList.add("trash-img");
+                trashBtn.src = "./img/trash_icon.svg";
                 li.appendChild(h4);
-                li.appendChild(check_btn);
-                li.appendChild(trash_btn);
-                task_list.insertBefore(li, task_list.firstChild);
+                li.appendChild(checkBtn);
+                li.appendChild(trashBtn);
+                taskList.insertBefore(li, taskList.firstChild);
             }
         });
     }
 };
 
-function add_element(text) {
+function addElement(text) {
     const li = document.createElement("li");
     const h4 = document.createElement("h4");
     h4.classList.add("task");
     h4.textContent = text;
-    const check_btn = document.createElement("img");
-    check_btn.classList.add("check-img");
-    check_btn.src = "./check_icon.svg";
-    const trash_btn = document.createElement("img");
-    trash_btn.classList.add("trash-img");
-    trash_btn.src = "./trash_icon.svg";
+    const checkBtn = document.createElement("img");
+    checkBtn.classList.add("check-img");
+    checkBtn.src = "./img/check_icon.svg";
+    const trashBtn = document.createElement("img");
+    trashBtn.classList.add("trash-img");
+    trashBtn.src = "./img/trash_icon.svg";
     li.appendChild(h4);
-    li.appendChild(check_btn);
-    li.appendChild(trash_btn);
-    task_list.insertBefore(li, task_list.firstChild);
+    li.appendChild(checkBtn);
+    li.appendChild(trashBtn);
+    taskList.insertBefore(li, taskList.firstChild);
 
 };
 
 
-function add_localStorage(item) {
+function addToLocalStorage(item) {
     let tasks;
     if (localStorage.getItem('tasks') === null) {
         tasks = []
@@ -221,10 +225,10 @@ function add_localStorage(item) {
 function change() {
     const e = document.getElementById('options').value;
     if (e === 'all') {
-        show_all_elements_ui();
-    } else if (e === "compleated") {
-        show_compleated_elements_ui()
-    } else if (e === "incompleated") {
-        show_incompleated_elements_ui();
+        showAllElementsUI();
+    } else if (e === "completed") {
+        showCompletedElementsUI();
+    } else if (e === "incompleted") {
+        showUncompletedElementsUI();
     };
 };
